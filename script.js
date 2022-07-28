6cf370f322217a58768d57cf8a253fcc19924262
Gabriel Marinho <gsmarinho@id.uff.br>
2022-07-28 20:57:02 -0300

    feat:button excluir

--- script.js
+++ script.js
@@ -2,23 +2,34 @@ const btnenvia = document.querySelector('.btn-envia')
 btnenvia.addEventListener("click", () => {
     const mensagensbox = document.querySelector('#local-mensagens')
     const texto = document.querySelector('.text')
+    if(texto.value.trim() == ""){
+        alert("Digite algo!!")
+    }else{
     const ul = document.createElement('ul')
     ul.classList.add("mensagens")
-    ul.innerHTML = `<li class="container">
-    <div class="escreve">
-        <p>${texto.value}</p>
-    </div>
-    <div class="button">
-        <button class="editar" onclick="edit(this)">Editar</button>
-        <button class="excluir" onclick="excluir(this)">Excluir</button>
-    </div>
-</li>`
+    ul.innerHTML = `
+    <li class="container">
+        <div class="escreve">
+            <p>${texto.value}</p>
+        </div>
+        <div class="button">
+            <button class="editar" onclick="edit(this)">Editar</button>
+            <button class="excluir" onclick="excluir(this)">Excluir</button>
+        </div>
+    </li>`
     mensagensbox.appendChild(ul)
     texto.value = ""
+    }
 })
-function edit(){
-
+function edit(check){
+    const text = document.querySelector(".text")
+    if(text.value.trim()==""){
+        alert("Digite algo!!")
+    }else{
+    check.parentNode.parentNode.firstElementChild.firstElementChild.innerHTML = text.value
+    text.value = ""
+    }
 }
 function excluir(check){
-    check.parentNode.parentNode.remove()
+    check.parentNode.parentNode.parentNode.remove()
 }
